511c0f35af7bda5b4f212eff4633a4a032bdd074
sria2000 <44519994+sria2000@users.noreply.github.com>
2026-06-24 22:45:10 +0100

    Add files via upload

---
+++ User_input.py
@@ -0,0 +1,9 @@
+# Accept Multiple User inputs
+
+First_name = input("Enter Users first name ")
+Last_name = input("Enter Users last name ")
+# abs will force to positive if negative is written
+age = abs(int(input("Enter  your age: ")))
+address = input("Enter your address with postcode ")
+
+full_details = input(f"Your full name is {First_name}  {Last_name} and you are {age} years old. You live in {address} \n")
